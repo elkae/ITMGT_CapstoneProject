c0e8c720b4e22bf6a84bc8361bde05842acd4732
Alwyn Ang <alwyn.ang@obf.ateneo.edu>
2023-07-18 10:31:19 +0800

    2nd commit

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,22 +1,48 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
 for (var i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'action:', action)
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
 
-		console.log('USER:', user)
-		if (user == 'AnonymousUser'){
-			console.log("Not logged in")
-		}else{
-			updateUserOrder(productId, action)
-		}
-	})
+        console.log('USER:', user)
+        if (user == 'AnonymousUser') {
+            addCookieItem(productId, action)
+        } else {
+            updateUserOrder(productId, action)
+        }
+    })
 }
 
 
-function updateUserOrder(productId, action){
+function addCookieItem(productId, action) {
+    console.log('Not logged in..')
+
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = { 'quantity': 1 }
+
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Remove Item')
+            delete cart[productId]
+
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+}
+
+function updateUserOrder(productId, action) {
     console.log('User is logged in, sending data...')
 
     var url = '/update_item/'
@@ -24,20 +50,20 @@ function updateUserOrder(productId, action){
     fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
 
         },
-        body: JSON.stringify({'productId': productId, 'action': action})
+        body: JSON.stringify({ 'productId': productId, 'action': action })
     })
 
-    .then((response) => {
-        return response.json()
-    })
+        .then((response) => {
+            return response.json()
+        })
 
-    .then((data) => {
-        console.log('data:', data)
-        location.reload()
-    })
+        .then((data) => {
+            console.log('data:', data)
+            location.reload()
+        })
 
 }
